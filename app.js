09e7de192bddeed58861012c22c732018cdf15c1
ursuladdd <anaclarasugajimin14@gmail.com>
2025-02-03 15:43:41 -0300

    Andamento

---
+++ app.js
@@ -0,0 +1,19 @@
+let number = document.querySelectorAll("#satifacao");
+let submit = document.getElementById("concluido");
+
+
+number.forEach(number => {
+    number.addEventListener('click', ()=> {
+      
+    })
+})
+
+
+
+
+
+submit.addEventListener('click', () => {
+   
+})
+
+
