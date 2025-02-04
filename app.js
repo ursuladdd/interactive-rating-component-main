12763123fdd1021f5610fdca1ded2431d0b282e8
ursuladdd <anaclarasugajimin14@gmail.com>
2025-02-04 15:19:24 -0300

    Andamento

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-let number = document.querySelectorAll("#satifacao");
-let submit = document.getElementById("concluido");
+document.addEventListener('DOMContentLoaded', () => {
+    let numbeSatiz = document.querySelectorAll("#satifacao");
+    let sumbt = document.getElementById("concluido");
+    let bannerCollection = document.getElementById("pai_container");
 
 
-number.forEach(number => {
-    number.addEventListener('click', ()=> {
-      
+    function papapa() {
+        for (let i = 0; i < numbeSatiz.length; i++) {
+            if(numbeSatiz[i].checked) {
+                return numbeSatiz[i].value
+
+            }
+            
+        }
+    }
+
+    sumbt.addEventListener('click', (event) => {
+        event.preventDefault();
+        bannerCollection.style.display = "none";
     })
+
 })
-
-
-
-
-
-submit.addEventListener('click', () => {
-   
-})
-
-
