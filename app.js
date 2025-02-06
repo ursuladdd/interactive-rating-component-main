1bc2bc59ecc7ec3dc335dccbe04afe2d5d2f5502
ursuladdd <anaclarasugajimin14@gmail.com>
2025-02-06 15:25:41 -0300

    Finalizando a parte do js :)

--- app.js
+++ app.js
@@ -18,8 +18,9 @@ document.addEventListener('DOMContentLoaded', () => {
         function validacaoForm(numbersSatisfied) {
             for (let i = 0; i < numbersSatisfied.length; i++) {
               if (numbersSatisfied[i].checked) {
-                return numbersSatisfied[i].value;
                 // Pelo menos um botão está marcado
+               let valorSelecionado = numbersSatisfied[i].value; //Armazena o valor selecionado
+              return valorSelecionado; //Retorna o valor selecionado
               }
             }
             alert("Avalie de 1 a 5, por favor");
@@ -30,8 +31,14 @@ document.addEventListener('DOMContentLoaded', () => {
             // Validação passou, o formulário pode ser enviado
             bannerCollection.style.display = "none"
             bannerThankYou.style.display= "flex";
+
+            //Obtém o valor retornado pela função
+            let valor = validacaoForm(numbersSatisfied);
+
+            select[0].innerHTML = `You selected ${valor} out of 5`
             
           }
+          form.reset();
 
     } )
 
