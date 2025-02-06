f5109962db3308a26fde1eed5981d4cbbc81ec4a
ursuladdd <anaclarasugajimin14@gmail.com>
2025-02-06 14:33:17 -0300

    Tentando melhorar

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ document.addEventListener('DOMContentLoaded', () => {
         function validacaoForm(numbersSatisfied) {
             for (let i = 0; i < numbersSatisfied.length; i++) {
               if (numbersSatisfied[i].checked) {
-                return numbersSatisfied[i].value;// Pelo menos um botão está marcado
+                return numbersSatisfied[i].value;
+                // Pelo menos um botão está marcado
               }
             }
             alert("Avalie de 1 a 5, por favor");
@@ -26,15 +27,10 @@ document.addEventListener('DOMContentLoaded', () => {
           }
         
           if (validacaoForm(numbersSatisfied)) {
-            
             // Validação passou, o formulário pode ser enviado
-            bannerCollection.style.display= "none";
+            bannerCollection.style.display = "none"
             bannerThankYou.style.display= "flex";
             
-          
-            
-          } else {
-            // Validação falhou, o formulário não será enviado
           }
 
     } )
